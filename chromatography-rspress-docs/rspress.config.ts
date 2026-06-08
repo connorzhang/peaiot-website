525b9090ccd2d94669785fc2f971db6944bd22ac
connorzhang <admin@16861.com>
2026-06-08 13:19:43 +0800

    fix: 修复 Rspress 侧边栏硬编码导致新项目文档被隐藏的问题，启用 _meta.json 自动路由发现机制

--- chromatography-rspress-docs/rspress.config.ts
+++ chromatography-rspress-docs/rspress.config.ts
@@ -14,46 +14,6 @@ export default defineConfig({
     nav: [
       { text: '产品手册目录', link: '/intro' },
     ],
-    sidebar: {
-      '/hardware/': [
-        {
-          text: '🏠 返回文档首页',
-          link: '/intro',
-        },
-        {
-          text: '🔬 硬件设备',
-          items: [
-            { text: '微型气相色谱系列', link: '/hardware/micro-gc/tcd-gas-flow' },
-          ],
-        }
-      ],
-      '/software/': [
-        {
-          text: '🏠 返回文档首页',
-          link: '/intro',
-        },
-        {
-          text: '💻 软件项目',
-          items: [
-            { text: '上位机工作站', link: '/software/workstation/intro' },
-          ],
-        }
-      ],
-      '/': [
-        {
-          text: '🏠 产品矩阵总览',
-          link: '/intro',
-        },
-        {
-          text: '🔬 硬件设备',
-          link: '/hardware/micro-gc/tcd-gas-flow',
-        },
-        {
-          text: '💻 软件项目',
-          link: '/software/workstation/intro',
-        }
-      ],
-    },
     footer: {
       message: '版权所有 © 2026 企业研发中心',
     },
